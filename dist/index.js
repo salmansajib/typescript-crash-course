96a1edb0c413c25b42fd36b853f805391b94041e
Salman-sajib <sfsajib@gmail.com>
2024-05-13 15:16:33 +0600

    tuple and tuple array

--- dist/index.js
+++ dist/index.js
@@ -8,3 +8,12 @@ let age;
 age = 32;
 let ids = [1, 2, 3, 4, 5];
 let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Bard', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+];
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,14 @@ age = 32;
 
 let ids: number[] = [1, 2, 3, 4, 5];
 let arr: any[] = [1, true, 'Hello'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Bard', true];
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, 'Brad'],
+  [2, 'John'],
+  [3, 'Jill'],
+];
